de3090590443e44bc9f67428b1b2c97e8e7b2cc6
wxqryy <vachitovaidar@gmail.com>
2026-01-13 23:40:35 +0500

    feat: moduleId in ExecuteOperationCommand

--- src/main/java/com/modula/common/domain/billing/dto/command/ExecuteOperationCommand.java
+++ src/main/java/com/modula/common/domain/billing/dto/command/ExecuteOperationCommand.java
@@ -3,15 +3,23 @@ package com.modula.common.domain.billing.dto.command;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Map;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExecuteOperationCommand {
+
     private UUID userId;
+
+    private UUID moduleId;
+
     private String operationKey;
+
     private String referenceId;
+
     private String description;
-    private java.util.Map<String, Object> parameters;
+
+    private Map<String, Object> parameters;
 }
