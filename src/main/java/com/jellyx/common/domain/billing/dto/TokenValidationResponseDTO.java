93d16cbd1077b13e2a940af907ab913d67974792
wxqryy <vachitovaidar@gmail.com>
2026-01-28 00:24:10 +0500

    feat(billing): added transactionId in TokenValidationResponseDTO

--- src/main/java/com/jellyx/common/domain/billing/dto/TokenValidationResponseDTO.java
+++ src/main/java/com/jellyx/common/domain/billing/dto/TokenValidationResponseDTO.java
@@ -4,30 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.UUID;
 
-/**
- * DTO для ответа валидации токенов.
- * Используется в core-builder и core-processor для передачи информации о
- * валидации и списании токенов.
- */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TokenValidationResponseDTO {
 
-    /**
-     * Статус операции (SUCCESS, FAILED, INSUFFICIENT_TOKENS, etc.)
-     */
     private String status;
 
-    /**
-     * Сообщение о результате операции
-     */
     private String message;
 
-    /**
-     * Количество токенов, которое было списано за операцию
-     */
     private Long tokensUsed;
+
+    private UUID transactionId;
 }
