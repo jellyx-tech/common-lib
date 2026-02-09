ac72ac20f230d109cfbdb81b10083a85376e56fc
wxqryy <vachitovaidar@gmail.com>
2026-02-09 19:23:54 +0500

    fix: added constructor in TokenBalanceDTO

--- src/main/java/com/jellyx/common/domain/billing/dto/TokenBalanceDTO.java
+++ src/main/java/com/jellyx/common/domain/billing/dto/TokenBalanceDTO.java
@@ -1,14 +1,25 @@
 package com.jellyx.common.domain.billing.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenBalanceDTO {
     private Long currentBalance;
     private Long totalEarned;
     private Long totalSpent;
     private LocalDateTime lastUpdated;
+
+    public TokenBalanceDTO(Long currentBalance, Long totalSpent, Long totalEarned) {
+        this.currentBalance = currentBalance;
+        this.totalSpent = totalSpent;
+        this.totalEarned = totalEarned;
+        this.lastUpdated = null;
+    }
 }
